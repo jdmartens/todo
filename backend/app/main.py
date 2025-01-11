a9b4fb43ade4126c5c7f48b6aa17d0ad99acc34a
jdmartens <jeremydmartens@gmail.com>
2025-01-11 16:06:13 -0600

    add cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from routes import tasks
 from services import dynamodb
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(tasks.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
