20836270bd3d5e92cb0b36eb72581cfd4b6b2b7b
jdmartens <jeremydmartens@gmail.com>
2025-01-11 16:10:54 -0600

    load and delete methods

--- frontend/src/app/task-list/task-list.component.ts
+++ frontend/src/app/task-list/task-list.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../task.service';
+import { MatTableModule } from '@angular/material/table';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatTableModule,
+    DatePipe,
+  ],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrls: ['./task-list.component.scss']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: any[] = [];
+  displayedColumns: string[] = ['task_name', 'due_date', 'status', 'actions'];
 
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe(
+      (data) => {
+        this.tasks = data;
+      },
+      (error) => {
+        console.error('Error fetching tasks:', error);
+      }
+    );
+  }
+
+  openAddTaskForm(): void {
+    // this.taskService.deleteTask(id).subscribe(
+    //   () => {
+    //     this.loadTasks();
+    //   },
+    //   (error) => {
+    //     console.error('Error deleting task:', error);
+    //   }
+    // );
+  }
+
+  editTask(id: string): void {
+    // this.taskService.deleteTask(id).subscribe(
+    //   () => {
+    //     this.loadTasks();
+    //   },
+    //   (error) => {
+    //     console.error('Error deleting task:', error);
+    //   }
+    // );
+  }
+
+  deleteTask(id: string): void {
+    this.taskService.deleteTask(id).subscribe(
+      () => {
+        this.loadTasks();
+      },
+      (error) => {
+        console.error('Error deleting task:', error);
+      }
+    );
+  }
 }
