76f1257c8f0ed48ebb3415f4894156d74e5f308d
jdmartens <jeremydmartens@gmail.com>
2025-01-11 16:08:37 -0600

    add routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/todos', pathMatch: 'full' },
+    { path: 'todos', component: TaskListComponent },
+    { path: 'todo/new', component: TaskFormComponent },
+    { path: 'todo/:id', component: TaskFormComponent }
+];
+
+  
