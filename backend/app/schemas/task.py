df2ee15b4d2ae50752654858072121cfca80f748
jdmartens <jeremydmartens@gmail.com>
2025-01-12 12:04:30 -0600

    add TaskUPdate schema

--- backend/app/schemas/task.py
+++ backend/app/schemas/task.py
@@ -4,6 +4,14 @@ from datetime import datetime
 class TaskCreate(BaseModel):
     task_name: str
     due_date: datetime
+    status: str
+    type: str
+
+class TaskUpdate(BaseModel):
+    id: str
+    task_name: str
+    due_date: datetime
+    status: str
     type: str
 
 class TaskResponse(BaseModel):
